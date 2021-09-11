406c2d4a73cf6ee6bfdb4e9f7870cb830602a2b5
Ben Niehoff <ben.niehoff@gmail.com>
2021-09-12 00:52:45 +0200

    Got unit testing set up and working

---
+++ lennardjones/tests/greeter/test_greeter.cpp
@@ -0,0 +1,21 @@
+/**
+ * A simple test of the friendly greeter system
+ */
+
+#include <catch2/catch.hpp>
+
+#include <src/greeter/greeter.hpp>
+
+#include <string>
+
+SCENARIO( "Interaction with the greeter" ) {
+    WHEN( "I give the greeter my name" ) {
+        std::string my_name{"Ben"};
+
+        THEN( "The greeter gives me a personalized greeting" ) {
+            std::string greeting = greet(my_name);
+
+            REQUIRE( "Hello, Ben, welcome to C++!" == greeting);
+        }
+    }
+}
