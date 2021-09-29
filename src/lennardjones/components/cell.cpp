278d0c4abb21761d3bebb054a0dc2a7cb988d343
Ben Niehoff <ben.niehoff@gmail.com>
2021-09-29 23:21:23 +0200

    Fixed typo in copyright notice

--- src/lennardjones/components/cell.cpp
+++ src/lennardjones/components/cell.cpp
@@ -15,6 +15,7 @@
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public
+ * License along with Lennard-Jones-Particles.  If not, see
+ * <https://www.gnu.org/licenses/>.
  */
