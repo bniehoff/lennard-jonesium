24dc260b6bd4d23fe6df33d01c12b3d76640539e
Ben Niehoff <ben.niehoff@gmail.com>
2021-10-12 13:25:14 +0200

    Changed design of Particle to prepare for enforcing invariants

--- tests/lennardjones/components/test_particle.cpp
+++ tests/lennardjones/components/test_particle.cpp
@@ -17,35 +17,33 @@ using components::Particle;
 
 SCENARIO( "Create and assign Particles" ) {
     AlignedVector3d position{1.618, 2.718, 3.142};
-    AlignedVector3d displacement{11.618, 12.718, 13.142};
     AlignedVector3d velocity{-3, -4, 2};
-    AlignedVector3d acceleration{0, 0, -9.8};
 
     WHEN( "I create a default Particle" ) {
         Particle p;
 
         THEN( "Its entries are all zero" ) {
-            REQUIRE( p.position.isZero() );
-            REQUIRE( p.displacement.isZero() );
-            REQUIRE( p.velocity.isZero() );
-            REQUIRE( p.acceleration.isZero() );
+            REQUIRE( p.position().isZero() );
+            REQUIRE( p.displacement().isZero() );
+            REQUIRE( p.velocity().isZero() );
+            REQUIRE( p.acceleration().isZero() );
         }
     }
 
     WHEN( "I create a Particle with some values" ) {
-        Particle q{position, displacement, velocity, acceleration};
+        Particle q{position, velocity};
 
         THEN( "Its entries match the values given" ) {
-            REQUIRE( position == q.position );
-            REQUIRE( displacement == q.displacement );
-            REQUIRE( velocity == q.velocity );
-            REQUIRE( acceleration == q.acceleration );
+            REQUIRE( position == q.position() );
+            REQUIRE( q.displacement().isZero() );
+            REQUIRE( velocity == q.velocity() );
+            REQUIRE( q.acceleration().isZero() );
         }
     }
 
     WHEN( "I assign one Particle to another" ) {
         Particle p;
-        Particle q{position, displacement, velocity, acceleration};
+        Particle q{position, velocity};
 
         p = q;
 
@@ -58,17 +56,17 @@ SCENARIO( "Create and assign Particles" ) {
         }
 
         THEN( "The Particles' values match" ) {
-            REQUIRE( q.position == p.position );
-            REQUIRE( q.displacement == p.displacement );
-            REQUIRE( q.velocity == p.velocity );
-            REQUIRE( q.acceleration == p.acceleration );
+            REQUIRE( q.position()     == p.position()     );
+            REQUIRE( q.displacement() == p.displacement() );
+            REQUIRE( q.velocity()     == p.velocity()     );
+            REQUIRE( q.acceleration() == p.acceleration() );
         }
     }
 
     WHEN( "I create several Particles" ) {
         Particle::reset_global_id();
         Particle p1, p2, p3, p4, p5;
-        Particle q{position, displacement, velocity, acceleration};
+        Particle q{position, velocity};
 
         THEN( "They are assigned IDs in sequence starting from zero" ) {
             REQUIRE( 0 == p1.id() );
