00517145df62e0f133b94880924a8ef55081040d
Ben Niehoff <ben.niehoff@gmail.com>
2021-10-20 15:23:52 +0200

    Added BoundaryCondition

---
+++ src/lennardjonesium/engine/boundary_condition.cpp
@@ -0,0 +1,23 @@
+/**
+ * boundary_condition.cpp
+ * 
+ * Copyright (c) 2021 Benjamin E. Niehoff
+ * 
+ * This file is part of Lennard-Jonesium.
+ * 
+ * Lennard-Jonesium is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Lennard-Jonesium is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with Lennard-Jonesium.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+
