9d4e3417a5eb19d42612a14c465651aadfc51808
Ben Niehoff <ben.niehoff@gmail.com>
2021-10-25 15:19:59 +0200

    Changed back to runtime polymorphism

---
+++ src/lennardjonesium/engine/integrator.cpp
@@ -0,0 +1,53 @@
+/**
+ * integrator.cpp
+ * 
+ * Copyright (c) 2021 Benjamin E. Niehoff
+ * 
+ * This file is part of Lennard-Jonesium.
+ * 
+ * Lennard-Jonesium is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Lennard-Jonesium is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with Lennard-Jonesium.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#include <lennardjonesium/engine/interaction.hpp>
+#include <lennardjonesium/engine/boundary_condition.hpp>
+#include <lennardjonesium/engine/integrator.hpp>
+
+namespace engine
+{
+    // Generic constructor allows nullptr entries
+    Integrator::Integrator(
+        double timestep,
+        const Interaction* interaction,
+        const BoundaryCondition* boundary_condition
+    )
+        : timestep_(timestep),
+          interaction_(interaction),
+          boundary_condition_(boundary_condition)
+    {}
+
+    // Create an integrator with the given time step, interaction, and boundary condition
+    Integrator::Integrator(
+        double timestep,
+        Interaction& interaction,
+        BoundaryCondition& boundary_condition
+    )
+        : Integrator::Integrator(timestep, &interaction, &boundary_condition)
+    {}
+
+    // Delegate to the above constructor with null interaction and boundary condition
+    Integrator::Integrator(double timestep)
+        : Integrator::Integrator(timestep, nullptr, nullptr)
+    {}
+} // namespace engine
---
+++ src/lennardjonesium/engine/velocity_verlet_integrator.cpp
@@ -0,0 +1,59 @@
+/**
+ * velocity_verlet_integrator.cpp
+ * 
+ * Copyright (c) 2021 Benjamin E. Niehoff
+ * 
+ * This file is part of Lennard-Jonesium.
+ * 
+ * Lennard-Jonesium is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Lennard-Jonesium is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with Lennard-Jonesium.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#include <lennardjonesium/physics/system_state.hpp>
+#include <lennardjonesium/physics/operator.hpp>
+#include <lennardjonesium/engine/velocity_verlet_integrator.hpp>
+
+namespace engine
+{
+    // Evolves time by one step
+    physics::SystemState& VelocityVerletIntegrator::operator() (physics::SystemState& state) const
+    {
+        /**
+         * The Velocity Verlet algorithm splits the integration into two half-steps, with the
+         * force calculation in between.
+         */
+
+        // First increment the velocities by half a time step:
+        state.velocities += (1./2.) * state.forces * timestep_;
+
+        // With the half-incremented velocities, give positions and displacements a full
+        // increment:
+        auto position_increment = state.velocities * timestep_;
+        state.positions += position_increment;
+        state.displacements += position_increment;
+
+        // Need to impose boundary conditions here
+        if (boundary_condition_ != nullptr)
+            state | (*boundary_condition_);
+
+        // Now with the new positions, compute the new forces
+        if (interaction_ != nullptr)
+            state | (*interaction_);
+
+        // Finally, with the new forces, increment the velocities by a second half-step:
+        state.velocities += (1./2.) * state.forces * timestep_;
+
+        return state;
+    }
+} // namespace engine
