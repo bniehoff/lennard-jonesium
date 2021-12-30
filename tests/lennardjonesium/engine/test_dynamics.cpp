174db0b6a2cd3c8237826d09ba35484e8bafb7c9
Ben Niehoff <ben.niehoff@gmail.com>
2021-12-30 20:34:04 +0100

    Added ConstantPairwiseForce and test

--- tests/lennardjonesium/engine/test_dynamics.cpp
+++ tests/lennardjonesium/engine/test_dynamics.cpp
@@ -1,5 +1,5 @@
 /**
- * Test for PeriodicBoundaryCondition
+ * Test for Dynamics
  */
 
 #include <catch2/catch.hpp>
@@ -9,6 +9,8 @@
 #include <src/lennardjonesium/physics/system_state.hpp>
 #include <src/lennardjonesium/tools/dimensions.hpp>
 #include <src/lennardjonesium/tools/cell_list_array.hpp>
+
+#include <tests/lennardjonesium/physics/constant_pairwise_force.hpp>
 
 // In order to test some protected methods, it is useful to define a derived class which makes
 // them public
@@ -39,7 +41,7 @@ SCENARIO("Imposing boundary conditions")
     tools::Dimensions dimensions{1.5};
 
     // Set up a PairwiseForce that represents the absence of forces
-    physics::ZeroPairwiseForce pairwise_force{0.5};
+    ConstantPairwiseForce pairwise_force{0, 0.5};
 
     // Set up the Dynamics object
     TestDynamics dynamics(dimensions, pairwise_force);
@@ -70,7 +72,7 @@ SCENARIO("Building/rebuilding the cell lists")
 
     // Set up the Dynamics object
     tools::Dimensions dimensions{1.5};
-    physics::ZeroPairwiseForce pairwise_force{0.5};
+    ConstantPairwiseForce pairwise_force{0, 0.5};
     TestDynamics dynamics(dimensions, pairwise_force);
 
     WHEN("I build the cell lists")
---
+++ tests/lennardjonesium/physics/test_constant_pairwise_force.cpp
@@ -0,0 +1,72 @@
+/**
+ * Test for ConstantPairwiseForce
+ */
+
+#include <catch2/catch.hpp>
+#include <Eigen/Dense>
+
+#include <src/lennardjonesium/physics/pairwise_force.hpp>
+
+#include <tests/lennardjonesium/physics/constant_pairwise_force.hpp>
+
+SCENARIO("Constant forces with cutoff distance")
+{
+    GIVEN("A constant force with strength 3.0 and cutoff distance 5.0")
+    {
+        double force = 3.0;
+        double cutoff_distance = 5.0;
+
+        ConstantPairwiseForce pairwise_force{force, cutoff_distance};
+
+        WHEN("I ask for the force at separation distance 2.0")
+        {
+            double distance = 2.0;
+
+            Eigen::Vector4d separation{0, 0, distance, 0};
+            physics::ForceContribution fc = pairwise_force(separation);
+
+            THEN("I get the expected values")
+            {
+                double expected_potential = force * (distance - cutoff_distance);
+                double expected_virial = -force * distance;
+
+                REQUIRE(Eigen::Vector4d{0, 0, -force, 0} == fc.force);
+                REQUIRE(expected_potential == fc.potential);
+                REQUIRE(expected_virial == fc.virial);
+            }
+        }
+
+        WHEN("I ask for the force at separation distance 2.0")
+        {
+            double distance = 4.0;
+
+            Eigen::Vector4d separation{0, 0, distance, 0};
+            physics::ForceContribution fc = pairwise_force(separation);
+
+            THEN("I get the expected values")
+            {
+                double expected_potential = force * (distance - cutoff_distance);
+                double expected_virial = -force * distance;
+
+                REQUIRE(Eigen::Vector4d{0, 0, -force, 0} == fc.force);
+                REQUIRE(expected_potential == fc.potential);
+                REQUIRE(expected_virial == fc.virial);
+            }
+        }
+
+        WHEN("I ask for the force at separation distance 8.0")
+        {
+            double distance = 8.0;
+
+            Eigen::Vector4d separation{0, 0, distance, 0};
+            physics::ForceContribution fc = pairwise_force(separation);
+
+            THEN("I get zero")
+            {
+                REQUIRE(Eigen::Vector4d::Zero() == fc.force);
+                REQUIRE(0 == fc.potential);
+                REQUIRE(0 == fc.virial);
+            }
+        }
+    }
+}
