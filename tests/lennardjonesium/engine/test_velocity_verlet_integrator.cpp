52512f884bda5d4073744157f214a4a2d399eb3e
Ben Niehoff <ben.niehoff@gmail.com>
2021-12-30 16:01:31 +0100

    Change style of test macros

--- tests/lennardjonesium/engine/test_velocity_verlet_integrator.cpp
+++ tests/lennardjonesium/engine/test_velocity_verlet_integrator.cpp
@@ -16,7 +16,8 @@ using engine::VelocityVerletIntegrator;
 using engine::PeriodicBoundaryCondition;
 using physics::SystemState;
 
-SCENARIO( "Inertial motion without forces" ) {
+SCENARIO("Inertial motion without forces")
+{
     SystemState state(2);
 
     // Set initial velocities
@@ -26,27 +27,32 @@ SCENARIO( "Inertial motion without forces" ) {
     // Configure integrator with time step 1
     auto integrator = VelocityVerletIntegrator(1.0);
 
-    WHEN( "I evolve the state by 4 time steps" ) {
+    WHEN("I evolve the state by 4 time steps")
+    {
         state | integrator | integrator | integrator | integrator;
 
-        THEN( "The positions move in the expected way" ) {
-            REQUIRE( Vector4d{4.0, 0, 0, 0} == state.positions.col(0) );
-            REQUIRE( Vector4d{0, 4.0, 0, 0} == state.positions.col(1) );
+        THEN("The positions move in the expected way")
+        {
+            REQUIRE(Vector4d{4.0, 0, 0, 0} == state.positions.col(0));
+            REQUIRE(Vector4d{0, 4.0, 0, 0} == state.positions.col(1));
         }
 
-        THEN( "The displacements move in the same way" ) {
-            REQUIRE( Vector4d{4.0, 0, 0, 0} == state.displacements.col(0) );
-            REQUIRE( Vector4d{0, 4.0, 0, 0} == state.displacements.col(1) );
+        THEN("The displacements move in the same way")
+        {
+            REQUIRE(Vector4d{4.0, 0, 0, 0} == state.displacements.col(0));
+            REQUIRE(Vector4d{0, 4.0, 0, 0} == state.displacements.col(1));
         }
 
-        THEN( "The velocities are unchanged" ) {
-            REQUIRE( Vector4d{1.0, 0, 0, 0} == state.velocities.col(0) );
-            REQUIRE( Vector4d{0, 1.0, 0, 0} == state.velocities.col(1) );
+        THEN("The velocities are unchanged")
+        {
+            REQUIRE(Vector4d{1.0, 0, 0, 0} == state.velocities.col(0));
+            REQUIRE(Vector4d{0, 1.0, 0, 0} == state.velocities.col(1));
         }
     }
 }
 
-SCENARIO( "Motion under a gravitational force" ) {
+SCENARIO("Motion under a gravitational force")
+{
     SystemState state(2);
 
     // Set initial velocities
@@ -59,30 +65,35 @@ SCENARIO( "Motion under a gravitational force" ) {
     // Configure integrator with time step 1
     auto integrator = VelocityVerletIntegrator(1.0);
 
-    WHEN( "I evolve the state by 4 time steps" ) {
+    WHEN("I evolve the state by 4 time steps")
+    {
         state | integrator | integrator | integrator | integrator;
 
         // Expected result from four iterations of Velocity Verlet with timestep 1.0
         constexpr double expected_z_coordinate = -((1./2.) + (3./2.) + (5./2.) + (7./2.));
 
-        THEN( "The positions move in the expected way" ) {
-            REQUIRE( Vector4d{4.0, 0, expected_z_coordinate, 0} == state.positions.col(0) );
-            REQUIRE( Vector4d{0, 4.0, expected_z_coordinate, 0} == state.positions.col(1) );
+        THEN("The positions move in the expected way")
+        {
+            REQUIRE(Vector4d{4.0, 0, expected_z_coordinate, 0} == state.positions.col(0));
+            REQUIRE(Vector4d{0, 4.0, expected_z_coordinate, 0} == state.positions.col(1));
         }
 
-        THEN( "The displacements move in the same way" ) {
-            REQUIRE( Vector4d{4.0, 0, expected_z_coordinate, 0} == state.displacements.col(0) );
-            REQUIRE( Vector4d{0, 4.0, expected_z_coordinate, 0} == state.displacements.col(1) );
+        THEN("The displacements move in the same way")
+        {
+            REQUIRE(Vector4d{4.0, 0, expected_z_coordinate, 0} == state.displacements.col(0));
+            REQUIRE(Vector4d{0, 4.0, expected_z_coordinate, 0} == state.displacements.col(1));
         }
 
-        THEN( "The velocities have accelerated downwards by the expected amount" ) {
-            REQUIRE( Vector4d{1.0, 0, -4.0, 0} == state.velocities.col(0) );
-            REQUIRE( Vector4d{0, 1.0, -4.0, 0} == state.velocities.col(1) );
+        THEN("The velocities have accelerated downwards by the expected amount")
+        {
+            REQUIRE(Vector4d{1.0, 0, -4.0, 0} == state.velocities.col(0));
+            REQUIRE(Vector4d{0, 1.0, -4.0, 0} == state.velocities.col(1));
         }
     }
 }
 
-SCENARIO( "Inertial motion with boundary conditions" ) {
+SCENARIO("Inertial motion with boundary conditions")
+{
     SystemState state(2);
 
     // Set initial velocities
@@ -97,27 +108,32 @@ SCENARIO( "Inertial motion with boundary conditions" ) {
         1.0, engine::null_force_calculation, boundary_condition
     );
 
-    WHEN( "I evolve the state by 4 time steps" ) {
+    WHEN("I evolve the state by 4 time steps")
+    {
         state | integrator | integrator | integrator | integrator;
 
-        THEN( "The positions move in the expected way" ) {
-            REQUIRE( Vector4d{1.0, 0, 0, 0} == state.positions.col(0) );
-            REQUIRE( Vector4d{0, 1.0, 0, 0} == state.positions.col(1) );
+        THEN("The positions move in the expected way")
+        {
+            REQUIRE(Vector4d{1.0, 0, 0, 0} == state.positions.col(0));
+            REQUIRE(Vector4d{0, 1.0, 0, 0} == state.positions.col(1));
         }
 
-        THEN( "The displacements show the total movement ignoring boundary conditions" ) {
-            REQUIRE( Vector4d{4.0, 0, 0, 0} == state.displacements.col(0) );
-            REQUIRE( Vector4d{0, 4.0, 0, 0} == state.displacements.col(1) );
+        THEN("The displacements show the total movement ignoring boundary conditions")
+        {
+            REQUIRE(Vector4d{4.0, 0, 0, 0} == state.displacements.col(0));
+            REQUIRE(Vector4d{0, 4.0, 0, 0} == state.displacements.col(1));
         }
 
-        THEN( "The velocities are unchanged" ) {
-            REQUIRE( Vector4d{1.0, 0, 0, 0} == state.velocities.col(0) );
-            REQUIRE( Vector4d{0, 1.0, 0, 0} == state.velocities.col(1) );
+        THEN("The velocities are unchanged")
+        {
+            REQUIRE(Vector4d{1.0, 0, 0, 0} == state.velocities.col(0));
+            REQUIRE(Vector4d{0, 1.0, 0, 0} == state.velocities.col(1));
         }
     }
 }
 
-SCENARIO( "Motion under a gravitational force with boundary conditions" ) {
+SCENARIO("Motion under a gravitational force with boundary conditions")
+{
     SystemState state(2);
 
     // Set initial velocities
@@ -135,26 +151,30 @@ SCENARIO( "Motion under a gravitational force with boundary conditions" ) {
         1.0, engine::null_force_calculation, boundary_condition
     );
 
-    WHEN( "I evolve the state by 4 time steps" ) {
+    WHEN("I evolve the state by 4 time steps")
+    {
         state | integrator | integrator | integrator | integrator;
 
         // Expected result from four iterations of Velocity Verlet with timestep 1.0
         const double expected_z_coordinate = 1.0;
         const double expected_z_displacement = -8.0; // -((1./2.) + (3./2.) + (5./2.) + (7./2.));
 
-        THEN( "The positions move in the expected way" ) {
-            REQUIRE( Vector4d{1.0, 0, expected_z_coordinate, 0} == state.positions.col(0) );
-            REQUIRE( Vector4d{0, 1.0, expected_z_coordinate, 0} == state.positions.col(1) );
+        THEN("The positions move in the expected way")
+        {
+            REQUIRE(Vector4d{1.0, 0, expected_z_coordinate, 0} == state.positions.col(0));
+            REQUIRE(Vector4d{0, 1.0, expected_z_coordinate, 0} == state.positions.col(1));
         }
 
-        THEN( "The displacements move in the same way" ) {
-            REQUIRE( Vector4d{4.0, 0, expected_z_displacement, 0} == state.displacements.col(0) );
-            REQUIRE( Vector4d{0, 4.0, expected_z_displacement, 0} == state.displacements.col(1) );
+        THEN("The displacements move in the same way")
+        {
+            REQUIRE(Vector4d{4.0, 0, expected_z_displacement, 0} == state.displacements.col(0));
+            REQUIRE(Vector4d{0, 4.0, expected_z_displacement, 0} == state.displacements.col(1));
         }
 
-        THEN( "The velocities have accelerated downwards by the expected amount" ) {
-            REQUIRE( Vector4d{1.0, 0, -4.0, 0} == state.velocities.col(0) );
-            REQUIRE( Vector4d{0, 1.0, -4.0, 0} == state.velocities.col(1) );
+        THEN("The velocities have accelerated downwards by the expected amount")
+        {
+            REQUIRE(Vector4d{1.0, 0, -4.0, 0} == state.velocities.col(0));
+            REQUIRE(Vector4d{0, 1.0, -4.0, 0} == state.velocities.col(1));
         }
     }
 }
--- tests/lennardjonesium/tools/test_dimensions.cpp
+++ tests/lennardjonesium/tools/test_dimensions.cpp
@@ -6,29 +6,34 @@
 
 #include <src/lennardjonesium/tools/dimensions.hpp>
 
-SCENARIO( "Constructing Dimensions objects" ) {
+SCENARIO("Constructing Dimensions objects")
+{
     double x{1.618};
     double y{2.718};
     double z{3.142};
     double s{1.414};
 
-    WHEN( "I create a Dimensions object with three different values" ) {
+    WHEN("I create a Dimensions object with three different values")
+    {
         auto d = tools::Dimensions(x, y, z);
 
-        THEN( "I see the correct values in the member variables" ) {
-            REQUIRE( x == d.x );
-            REQUIRE( y == d.y );
-            REQUIRE( z == d.z );
+        THEN("I see the correct values in the member variables")
+        {
+            REQUIRE(x == d.x);
+            REQUIRE(y == d.y);
+            REQUIRE(z == d.z);
         }
     }
 
-    WHEN( "I create a Dimensions object from a single value" ) {
+    WHEN("I create a Dimensions object from a single value")
+    {
         auto d = tools::Dimensions(s);
 
-        THEN( "I see the same value in all three member variables" ) {
-            REQUIRE( s == d.x );
-            REQUIRE( s == d.y );
-            REQUIRE( s == d.z );
+        THEN("I see the same value in all three member variables")
+        {
+            REQUIRE(s == d.x);
+            REQUIRE(s == d.y);
+            REQUIRE(s == d.z);
         }
     }
 }
