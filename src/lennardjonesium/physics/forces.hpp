fa764332b8db401e81cd5ae2945e24b0913639c4
Ben Niehoff <ben.niehoff@gmail.com>
2022-01-04 21:39:13 +0100

    Removed square_cutoff_distance()

--- src/lennardjonesium/physics/forces.hpp
+++ src/lennardjonesium/physics/forces.hpp
@@ -79,9 +79,6 @@ namespace physics
 
             // Get the cutoff distance
             virtual double cutoff_distance() const = 0;
-
-            // Get the square of the cutoff distance (useful for avoiding square roots)
-            virtual double square_cutoff_distance() const = 0;
     };
 
     class BackgroundForce
