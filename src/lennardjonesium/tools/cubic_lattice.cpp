111c3b26b21dfe1fa3877ccd2182746f92b09edd
Ben Niehoff <ben.niehoff@gmail.com>
2022-01-20 20:28:42 +0100

    Fix scale factor calculation
    Originally we were computing the density of the prototype crystal
    lattice with the assumption that it was the same as the density of an
    individual unit cell.  However, if the cubical box is underfilled, this
    is no longer true.  We have fixed this calculation to use the total
    particle count and the total volume in a straightforward way.
    
    Also, we move these computations to the body of the constructor for
    clarity.

--- src/lennardjonesium/tools/cubic_lattice.cpp
+++ src/lennardjonesium/tools/cubic_lattice.cpp
@@ -35,13 +35,28 @@ namespace tools
 {
     CubicLattice::CubicLattice
         (int particle_count, double density, CubicLattice::UnitCell unit_cell)
-        : unit_cell_{unit_cell},
-          scale_{std::cbrt(unit_cell.density<double>() / density)},
-          particle_count_{particle_count},
-          cells_per_side_{static_cast<int>(std::ceil(
-              std::cbrt(static_cast<double>(particle_count) / unit_cell.density<double>())
-          ))}
-    {}
+        : unit_cell_{unit_cell}, particle_count_{particle_count}
+    {
+        // Still need to compute scale_factor_ and cells_per_side_.
+
+        // First find the total number of nonempty cells
+        int nonempty_cells = std::ceil(
+            static_cast<double>(particle_count) / static_cast<double>(unit_cell.cols())
+        );
+
+        // cells_per_side_ is the cube root of the smallest cube that contains this number of cells
+        cells_per_side_ = std::ceil(std::cbrt(static_cast<double>(nonempty_cells)));
+
+        // Next find the prototype density, given by the number of particles divided by volume,
+        // assuming that a unit cell has unit volume
+        double prototype_density = (
+            static_cast<double>(particle_count) /
+            static_cast<double>(cells_per_side_ * cells_per_side_ * cells_per_side_)
+        );
+
+        // scale_factor_ is then the linear scale needed to change to the desired density
+        scale_factor_ = std::cbrt(prototype_density / density);
+    }
 
     std::generator<Eigen::Vector4d> CubicLattice::operator() ()
     {
@@ -51,22 +66,22 @@ namespace tools
              * To get the coordinates of the current lattice site, we note that the index can be
              * decomposed in the following way:
              * 
-             *  index = ((x * cells_per_side_ + y) * cells_per_side_ + z) * unit_cell.density + s
+             *  index = ((x * cells_per_side_ + y) * cells_per_side_ + z) * unit_cell.cols() + s
              * 
              * where (x, y, z) is the coordinate of the base point of the lattice cell, and then s
              * gives the index of the specific site within the lattice cell.  We can obtain
              * (x, y, z, s) by a sequence of quotient-remainder operations.
              */
-            auto xyz_s = std::div(index, unit_cell_.density<int>());
+
+            // We need the cast because Eigen::Index is a long int
+            auto xyz_s = std::div(index, static_cast<int>(unit_cell_.cols()));
             auto xy_z = std::div(xyz_s.quot, cells_per_side_);
             auto x_y = std::div(xy_z.quot, cells_per_side_);
 
             // Now we can construct the lattice coordinate
             Eigen::Vector4i lattice_cell {x_y.quot, x_y.rem, xy_z.rem, 0};
 
-            co_yield (
-                lattice_cell.cast<double>() + unit_cell_.lattice_sites.col(xyz_s.rem)
-            ) * scale_;
+            co_yield (lattice_cell.cast<double>() + unit_cell_.col(xyz_s.rem)) * scale_factor_;
         }
     }
 } // namespace tools
