f22600c681204dda6e94b1349065d19e82dee83f
Ben Niehoff <ben.niehoff@gmail.com>
2022-01-22 21:38:48 +0100

    Add remaining tests for CubicLattice
    The second set of tests for CubicLattice no longer cause alignment
    assertion errors.  These tests verify that the particles will be placed
    at the correct specific locations in the crystal lattice structure.

--- tests/lennardjonesium/tools/test_cubic_lattice.cpp
+++ tests/lennardjonesium/tools/test_cubic_lattice.cpp
@@ -76,31 +76,77 @@ SCENARIO("Producing particle lattices with the correct density")
     }
 }
 
-// SCENARIO("Producing particle lattices with the correct lattice sites")
-// {
-//     int particle_count{7};
-//     double desired_density{7.0 / 8.0};
+SCENARIO("Producing particle lattices with the correct lattice sites")
+{
+    int particle_count{7};
+    double desired_density{7.0 / 8.0};
 
-//     WHEN("I create a simple cubic lattice")
-//     {
-//         tools::CubicLattice lattice(
-//             particle_count, desired_density, tools::CubicLattice::Simple()
-//         );
+    WHEN("I create a simple cubic lattice")
+    {
+        tools::CubicLattice lattice(
+            particle_count, desired_density, tools::CubicLattice::Simple()
+        );
 
-//         std::vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> expected_sites{
-//             {0, 0, 0, 0}, {0, 0, 1, 0}, {0, 1, 0, 0}, {0, 1, 1, 0},
-//             {1, 0, 0, 0}, {1, 0, 1, 0}, {1, 1, 0, 0}
-//         };
+        std::vector<Eigen::Vector4d> expected_sites{
+            {0, 0, 0, 0}, {0, 0, 1, 0}, {0, 1, 0, 0}, {0, 1, 1, 0},
+            {1, 0, 0, 0}, {1, 0, 1, 0}, {1, 1, 0, 0}
+        };
 
-//         // std::vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> result_sites{};
-//         // for (auto site : lattice())
-//         // {
-//         //     result_sites.push_back(site);
-//         // }
+        std::vector<Eigen::Vector4d> result_sites{};
+        for (auto site : lattice())
+        {
+            result_sites.push_back(site);
+        }
 
-//         // THEN("I get the correct lattice sites")
-//         // {
-//         //     REQUIRE_THAT(result_sites, Catch::UnorderedEquals(expected_sites));
-//         // }
-//     }
-// }
+        THEN("I get the correct lattice sites")
+        {
+            REQUIRE_THAT(result_sites, Catch::UnorderedEquals(expected_sites));
+        }
+    }
+
+    WHEN("I create a body-centered cubic lattice")
+    {
+        tools::CubicLattice lattice(
+            particle_count, desired_density, tools::CubicLattice::BodyCentered()
+        );
+
+        std::vector<Eigen::Vector4d> expected_sites{
+            {0, 0, 0, 0}, {0.5, 0.5, 0.5, 0}, {0, 0, 1, 0}, {0.5, 0.5, 1.5, 0},
+            {0, 1, 0, 0}, {0.5, 1.5, 0.5, 0}, {0, 1, 1, 0}
+        };
+
+        std::vector<Eigen::Vector4d> result_sites{};
+        for (auto site : lattice())
+        {
+            result_sites.push_back(site);
+        }
+
+        THEN("I get the correct lattice sites")
+        {
+            REQUIRE_THAT(result_sites, Catch::UnorderedEquals(expected_sites));
+        }
+    }
+
+    WHEN("I create a face-centered cubic lattice")
+    {
+        tools::CubicLattice lattice(
+            particle_count, desired_density, tools::CubicLattice::FaceCentered()
+        );
+
+        std::vector<Eigen::Vector4d> expected_sites{
+            {0, 0, 0, 0}, {0.5, 0.5, 0, 0}, {0.5, 0, 0.5, 0}, {0, 0.5, 0.5, 0},
+            {0, 0, 1, 0}, {0.5, 0.5, 1, 0}, {0.5, 0, 1.5, 0}
+        };
+
+        std::vector<Eigen::Vector4d> result_sites{};
+        for (auto site : lattice())
+        {
+            result_sites.push_back(site);
+        }
+
+        THEN("I get the correct lattice sites")
+        {
+            REQUIRE_THAT(result_sites, Catch::UnorderedEquals(expected_sites));
+        }
+    }
+}
