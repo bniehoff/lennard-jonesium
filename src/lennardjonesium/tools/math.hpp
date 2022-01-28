8da0ba6bd77a392034ef215ce68537f81f173901
Ben Niehoff <ben.niehoff@gmail.com>
2022-01-28 22:05:35 +0100

    Add Equilibrator
    The Equilibrator attempts to bring the state to equilibrium at the
    desired temperature.  This still needs testing.

---
+++ src/lennardjonesium/tools/math.hpp
@@ -0,0 +1,39 @@
+/**
+ * math.hpp
+ * 
+ * Copyright (c) 2021-2022 Benjamin E. Niehoff
+ * 
+ * This file is part of Lennard-Jonesium.
+ * 
+ * Lennard-Jonesium is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Lennard-Jonesium is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with Lennard-Jonesium.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LJ_MATH_HPP
+#define LJ_MATH_HPP
+
+#include <cmath>
+
+namespace tools
+{
+    /**
+     * Here we provide some basic math functions that are universally useful.
+     */
+
+    bool within_tolerance(double measured, double desired, double tolerance)
+    {return std::abs(measured / desired - 1.0) < tolerance;}
+    
+} // namespace tools
+
+#endif
