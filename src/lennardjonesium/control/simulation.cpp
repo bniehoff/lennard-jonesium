48beaf04c23d29295fae319c7d46888c18bddefc
Ben Niehoff <ben.niehoff@gmail.com>
2022-03-08 21:09:01 +0100

    Add basic simulation loop
    Have written the basic simulation loop which should have the necessary
    logic to actually run the simulation (still needs to be tested).
    
    Have not yet added anything related to logging.

--- src/lennardjonesium/control/simulation.cpp
+++ src/lennardjonesium/control/simulation.cpp
@@ -21,11 +21,22 @@
  */
 
 #include <variant>
+#include <queue>
 
 #include <lennardjonesium/physics/system_state.hpp>
+#include <src/lennardjonesium/physics/transformations.hpp>
+#include <src/lennardjonesium/physics/measurements.hpp>
 #include <lennardjonesium/engine/integrator.hpp>
 #include <lennardjonesium/control/simulation_phase.hpp>
 #include <lennardjonesium/control/simulation.hpp>
+
+namespace
+{
+    // Define an overload template for visiting the Command variant
+    template<class... Lambdas>
+    struct Overloaded : Lambdas... { using Lambdas::operator()...; };
+} // namespace
+
 
 namespace control
 {
@@ -34,13 +45,79 @@ namespace control
         // Clock for counting the global time
         int time_step = 0;
 
+        // Measuring device to get the instantaneous thermodynamic information
+        physics::ThermodynamicMeasurement measurement;
+
         // Initialize the first SimulationPhase
         simulation_phases_.front()->set_start_time(time_step);
 
-        // Main simulation loop
-        while (true)
+        // Prepare a queue of Commands to be received from the SimulationPhase
+        std::queue<Command> commands;
+
+        // Signal to tell us when to exit the loop
+        bool running = true;
+
+        // Create the visitor object once
+        auto command_interpreter = Overloaded
         {
-            break;
+            [&](const RecordObservation& command [[maybe_unused]])
+            {
+                // Need to use the _previous_ time step here, since time has already incremented
+            },
+
+            [&](const AdjustTemperature& command)
+            {
+                state | physics::set_temperature(command.temperature);
+            },
+
+            [&](const PhaseComplete& command [[maybe_unused]])
+            {
+                this->simulation_phases_.pop();
+
+                if (this->simulation_phases_.empty())
+                {
+                    // End the simulation if there are no more phases
+                    running = false;
+                }
+                else
+                {
+                    // Initialize the new phase with the time step and start a new command queue
+                    this->simulation_phases_.front()->set_start_time(time_step);
+                    commands = {};
+                }
+            },
+
+            [&](const AbortSimulation& command [[maybe_unused]])
+            {
+                running = false;
+            }
+        };
+        
+        // Main simulation loop
+        while (running)
+        {
+            /**
+             * We use a two-state loop structure that either advances the simulation by one time
+             * step OR processes the commands from the SimulationPhase.  This allows us to process
+             * a queue of multiple commands, while having only one level of loop nesting.
+             * 
+             * This makes it much easier to break out of the loop when the simulation ends.  It also
+             * helps make sure that we fully process one command at a time (and, for example, not
+             * process any additional commands after an AbortSimulation command, even if there may
+             * be some in the queue).
+             */
+
+            if (commands.empty())
+            {
+                state | integrator_ | measurement;
+                commands = simulation_phases_.front()->evaluate(time_step, measurement);
+                ++time_step;
+            }
+            else
+            {
+                std::visit(command_interpreter, commands.front());
+                commands.pop();
+            }
         }
 
         return state;
