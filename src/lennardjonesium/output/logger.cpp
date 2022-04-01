fc40d33b31d37609d9baf49fb97e537856058ee6
Ben Niehoff <ben.niehoff@gmail.com>
2022-04-01 17:13:46 +0200

    Add logger and test_logger
    The Logger encapsulates the logging functions, the message queue, and
    the asynchronous consumer thread that actually writes to files.  Now
    a producer thread (which may be the main thread) can simply log
    messages with the log() method.

---
+++ src/lennardjonesium/output/logger.cpp
@@ -0,0 +1,92 @@
+/**
+ * logger.cpp
+ * 
+ * Copyright (c) 2021-2022 Benjamin E. Niehoff
+ * 
+ * This file is part of Lennard-Jonesium.
+ * 
+ * Lennard-Jonesium is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Lennard-Jonesium is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with Lennard-Jonesium.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <utility>
+#include <thread>
+
+#include <lennardjonesium/tools/message_buffer.hpp>
+#include <lennardjonesium/output/log_message.hpp>
+#include <lennardjonesium/output/sinks.hpp>
+#include <lennardjonesium/output/dispatcher.hpp>
+#include <lennardjonesium/output/logger.hpp>
+
+namespace output
+{
+    Logger::Logger(
+        std::ostream& event_log,
+        std::ostream& thermodynamic_log,
+        std::ostream& observation_log
+    )
+        : event_sink_{event_log},
+          thermodynamic_sink_{thermodynamic_log},
+          observation_sink_{observation_log}
+    {
+        // Initialize the log files
+        event_sink_.write_header();
+        thermodynamic_sink_.write_header();
+        observation_sink_.write_header();
+
+        event_sink_.flush();
+        thermodynamic_sink_.flush();
+        observation_sink_.flush();
+
+        // Start the consumer thread
+        consumer_ = std::thread(
+            [this]() {
+                Dispatcher dispatcher{
+                    this->event_sink_,
+                    this->thermodynamic_sink_,
+                    this->observation_sink_
+                };
+
+                while (auto o = this->buffer_.get())
+                {
+                    auto [time_step, message] = o.value();
+                    dispatcher.send(time_step, message);
+                }
+
+                dispatcher.flush_all();
+            }
+        );
+    }
+
+    void Logger::close()
+    {
+        // If consumer thread is running, then close the buffer
+        // and wait for consumer thread to finish
+
+        if (consumer_.joinable())
+        {
+            buffer_.close();
+            consumer_.join();
+        }
+    }
+
+    Logger::~Logger() noexcept
+    {
+        // Try to close the buffer
+        try {close();}
+        catch(...) {}
+    }
+} // namespace output
+
---
+++ src/lennardjonesium/output/logger.hpp
@@ -0,0 +1,79 @@
+/**
+ * logger.hpp
+ * 
+ * Copyright (c) 2021-2022 Benjamin E. Niehoff
+ * 
+ * This file is part of Lennard-Jonesium.
+ * 
+ * Lennard-Jonesium is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Lennard-Jonesium is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with Lennard-Jonesium.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LJ_LOGGER_HPP
+#define LJ_LOGGER_HPP
+
+#include <iostream>
+#include <utility>
+#include <thread>
+
+#include <lennardjonesium/tools/message_buffer.hpp>
+#include <lennardjonesium/output/log_message.hpp>
+#include <lennardjonesium/output/sinks.hpp>
+
+namespace output
+{
+    class Logger
+    {
+        /**
+         * The Logger prints the relevant simulation data to output streams.  It is not a flexible
+         * and generic logging system like the Boost logging library, nor is it intended for
+         * diagnostic output.  Its main purpose is to record the actual physics data that is
+         * produced in the course of simulation.
+         * 
+         * NOTE: The files it produces are meant to be permanent (not rotated like system logs).
+         * The Logger accepts std::ostream& arguments to its constructor; the caller is responsible
+         * for configuring these streams and making sure they point to opened files.  The caller
+         * is also responsible for closing the streams afterward.
+         */
+
+        public:
+            Logger(
+                std::ostream& event_log,
+                std::ostream& thermodynamic_log,
+                std::ostream& observation_log
+            );
+
+            // Used by producer thread to send log messages, which will be dispatched to the
+            // appropriate destination
+            void log(int time_step, LogMessage message) {buffer_.put({time_step, message});}
+
+            // Call close() after producer threads are finished, this clears the message buffer
+            // and terminates consumer thread (optional)
+            // Note that there is no way to reopen logging
+            void close();
+
+            // If close() was not called, perform the same operations when we go out of scope
+            ~Logger() noexcept;
+        
+        private:
+            EventSink event_sink_;
+            ThermodynamicSink thermodynamic_sink_;
+            ObservationSink observation_sink_;
+            tools::MessageBuffer<std::pair<int, LogMessage>> buffer_;
+            std::thread consumer_;
+    };
+} // namespace output
+
+
+#endif
