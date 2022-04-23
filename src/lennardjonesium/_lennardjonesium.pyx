5ca294a3e57acd8e6c39fcab91b48a4cda80e41a
Ben Niehoff <ben.niehoff@gmail.com>
2022-04-23 16:15:13 +0200

    Experiment with Scikit-Build
    Made sure I understand how to compile Python/Cython projects with
    directory structures configured in various ways.

--- src/lennardjonesium/_lennardjonesium.pyx
+++ src/lennardjonesium/_lennardjonesium.pyx
@@ -1,4 +1,29 @@
+"""
+_lennardjonesium.pyx
+
+Copyright (c) 2021-2022 Benjamin E. Niehoff
+
+This file is part of Lennard-Jonesium.
+
+Lennard-Jonesium is free software: you can redistribute
+it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+Lennard-Jonesium is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with Lennard-Jonesium.  If not, see
+<https://www.gnu.org/licenses/>.
+"""
+
 from lennardjonesium._lennardjonesium cimport _Greeter
+
+def hello_python(name):
+    print("Hello, {}, this is a boring function from Python".format(name))
 
 # Cython greeter example
 cpdef void hello_cython(str name):
