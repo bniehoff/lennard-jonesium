ba2479fdaa52f0c7baed2747af00f4255cda97bf
Ben Niehoff <ben.niehoff@gmail.com>
2022-05-14 15:47:43 +0200

    Add capability to read/write configuration files
    Python's configparser module makes this easy, simply added some methods
    to Configuration to enabled reading and writing files.

--- src/python/lennardjonesium/simulation/configuration.py
+++ src/python/lennardjonesium/simulation/configuration.py
@@ -22,20 +22,27 @@ License along with Lennard-Jonesium.  If not, see
 
 
 from dataclasses import dataclass
+import configparser
 
 from lennardjonesium.simulation._seed_generator import SeedGenerator
 
 
 @dataclass
 class Configuration:
+    """
+    The Configuration holds all of the parameters needed to run a single Simulation.  These are
+    stored in a convenient nested dataclass structure.  We also provide some methods for reading
+    and writing config files, as well as conversion to a Configuration from a simple nested
+    dictionary.
+    """
     @dataclass
     class _System:
         temperature: float = 0.8
         density: float = 1.0
         particle_count: int = 100
-        random_seed: int = SeedGenerator.default_seed()
         cutoff_distance: float = 2.5
         time_delta: float = 0.005
+        random_seed: int = SeedGenerator.default_seed()
     
     @dataclass
     class _Equilibration:
@@ -65,3 +72,89 @@ class Configuration:
     equilibration: _Equilibration = _Equilibration()
     observation: _Observation = _Observation()
     filepaths: _Filepaths = _Filepaths()
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        """
+        Creates a Configuration object from a nested dictionary.
+        """
+        c = cls()
+        c.read_dict(d)
+        return c
+    
+    @classmethod
+    def from_config_parser(cls, cp: configparser.ConfigParser):
+        """
+        Creates a Configuration object from a ConfigParser object.
+        """
+        c = cls()
+        c.read_config_parser(cp)
+        return c
+    
+    @classmethod
+    def from_file(cls, filename: str):
+        """
+        Creates a Configuration object from a config file
+        """
+        c = cls()
+        c.read(filename)
+        return c
+    
+    def read_dict(self, d: dict):
+        """
+        Sets the fields based on values from a dictionary.
+        """
+        # We only look for the keys that are actually defined in the dataclass, so if the
+        # dictionary contains additional keys, they will be ignored
+        for section in self.__dict__:
+            if section in d:
+                for key in self.__getattribute__(section).__dict__:
+                    if key in d[section]:
+                        self.__getattribute__(section).__setattr__(key, d[section][key])
+    
+    def read_config_parser(self, cp: configparser.ConfigParser):
+        """
+        Sets the fields based on values from a ConfigParser (not quite the same as a dictionary).
+        """
+        # We only look for the keys that are actually defined in the dataclass, so if the
+        # dictionary contains additional keys, they will be ignored
+        for section in self.__dict__:
+            if section in cp:
+                for key in self.__getattribute__(section).__dict__:
+                    if key in cp[section]:
+                        # We need to use different methods to automatically parse the values into
+                        # the appropriate type
+                        field_type = type(self.__getattribute__(section).__getattribute__(key))
+
+                        if field_type is int:
+                            value = cp.getint(section, key)
+                        elif field_type is float:
+                            value = cp.getfloat(section, key)
+                        elif field_type is bool:
+                            value = cp.getboolean(section, key)
+                        else:
+                            value = cp.get(section, key)
+
+                        self.__getattribute__(section).__setattr__(key, value)
+
+    def read(self, filename: str):
+        """
+        Reads a Configuration from a file
+        """
+        parser = configparser.ConfigParser()
+        parser.read(filename)
+        self.read_config_parser(parser)
+    
+    def write(self, filename: str):
+        """
+        Writes a Configuration to a file
+        """
+        parser = configparser.ConfigParser()
+
+        for section in self.__dict__:
+            parser.add_section(section)
+            for key in self.__getattribute__(section).__dict__:
+                parser[section][key] = str(self.__getattribute__(section).__getattribute__(key))
+        
+        with open(filename, 'w') as outfile:
+            parser.write(outfile)
